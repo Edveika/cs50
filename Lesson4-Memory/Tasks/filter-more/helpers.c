05712bb70e5686445dd76efc3461b703be2ec124
Edveika <edvinasbureika@gmail.com>
2023-09-04 23:28:14 +0300

    Greyscale filter

---
+++ Lesson4-Memory/Tasks/filter-more/helpers.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+#include "helpers.h"
+
+int max(int x, int y)
+{
+    return (x > y) ? x : y;
+}
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; ++i)
+    {
+        for (int j = 0; j < width; ++j)
+        {
+            int biggest_value = max(image[i][j].rgbtRed, image[i][j].rgbtGreen);
+            biggest_value = max(biggest_value, image[i][j].rgbtBlue);
+
+            image[i][j].rgbtRed = biggest_value;
+            image[i][j].rgbtGreen = biggest_value;
+            image[i][j].rgbtBlue = biggest_value;
+        }
+    }
+
+    return;
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; ++i)
+    {
+        for (int j = 0; j < width; ++j)
+        {
+        }
+    }
+
+    return;
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; ++i)
+    {
+        for (int j = 1; j < width; ++j)
+        {
+        }
+    }
+
+    return;
+}
+
+// Detect edges
+void edges(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; ++i)
+    {
+        for (int j = 0; j < width; ++j)
+        {
+        }
+    }
+
+    return;
+}
