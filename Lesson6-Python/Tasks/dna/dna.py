419c34407e39233e3968a3dafc31b4a1d9f91126
Edveika <edvinasbureika@gmail.com>
2023-09-20 20:03:54 +0300

    DNA task complete
    Little bit of help from gpt used

--- Lesson6-Python/Tasks/dna/dna.py
+++ Lesson6-Python/Tasks/dna/dna.py
@@ -4,31 +4,41 @@ import sys
 def main():
 
     # Check for command-line usage
-    # if len(sys.argv) != 3:
-    #     print("Usage: ./dna database sequence")
-    #     exit()
+    if len(sys.argv) != 3:
+        print("Usage: ./dna database sequence")
+        exit()
 
     # Read database file into a variable
-    with open("/home/edveika/Programming/cs50/Lesson6-Python/Tasks/dna/databases/large.csv", "r") as database_file:
+    with open(sys.argv[1], "r") as database_file:
         reader = csv.reader(database_file)
-        next(reader)
+        #next(reader)
         database = []
         for row in reader:
-            database.extend(row)
-            print(row)
+            database.append(row)
             
-
     # Read DNA sequence file into a variable
-    with open("/home/edveika/Programming/cs50/Lesson6-Python/Tasks/dna/sequences/5.txt", "r") as sequence_file:
+    with open(sys.argv[2], "r") as sequence_file:
         dna_sequence = sequence_file.read()
-        print(dna_sequence)
 
-    # Find longest match of each STR in DNA sequence
-    longest_run = longest_match(database, dna_sequence)
-    #print(longest_run)
+    sub_sequences = {}
+    # Gets name of dnas and fills counters
+    for column_name in database[0][1:]:
+        sub_sequences[column_name] = 0
+
+    for sub_sequence in sub_sequences:
+        # Find longest match of each STR in DNA sequence
+        longest_run = longest_match(dna_sequence, sub_sequence)
+        sub_sequences[sub_sequence] = longest_run
 
     # Check database for matching profiles
-
+    for row in database[1:]:
+        # Had to use chatgpt for this one, confusing...
+        # Basically it creates data dict like sub_sequences, excludes name from the row
+        row_data = {k: int(v) for k, v in zip(sub_sequences.keys(), row[1:])}
+        if row_data == sub_sequences:
+            print(row[0])
+            exit()
+        
     return
 
 
@@ -50,7 +60,6 @@ def longest_match(sequence, subsequence):
         # If a match, move substring to next potential match in sequence
         # Continue moving substring and checking for matches until out of consecutive matches
         while True:
-
             # Adjust substring start and end
             start = i + count * subsequence_length
             end = start + subsequence_length
